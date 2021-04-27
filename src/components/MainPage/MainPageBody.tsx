cad8808a4649c3e323d95face364e8f832af5836
s369236s <s369236s@gmail.com>
2021-04-27 23:47:53 +0800

    ?_?

--- src/components/MainPage/MainPageBody.tsx
+++ src/components/MainPage/MainPageBody.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 import { CanvasImage } from "./CanvasImage";
@@ -5,12 +6,19 @@ import { IntroFeatuer } from "./IntroFeatuer";
 import { MainPageRestaurantGrids } from "./MainPageRestaurantGrids";
 
 export const MainPageBody: React.FC = ({}) => {
+  const loginSubmit = () => {
+    console.log(213);
+    axios
+      .get("http://111.185.146.121:80/api/test.php")
+      .then((res) => console.log(res));
+  };
   return (
     <main className="main-page-body-container">
       <CanvasImage />
       <IntroFeatuer />
       <Link to="/">桃園的美味早餐</Link>
       <MainPageRestaurantGrids />
+      <button onClick={loginSubmit}>123</button>
     </main>
   );
 };
