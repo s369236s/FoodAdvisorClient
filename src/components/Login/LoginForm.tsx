ed50e58b3a9a5f3d2b7fe74459c9d638b5e46384
unknown <s369236s@gmail.com>
2021-04-29 18:35:19 +0800

    ?_?

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,9 +1,24 @@
-import React, { FormEvent } from "react";
+import axios from "axios";
+import React, { FormEvent, useState } from "react";
 interface Props {
   switchSubmit: (e: React.FormEvent<HTMLButtonElement>) => void;
 }
 export const LoginForm: React.FC<Props> = ({ switchSubmit }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const loginSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    // console.log(email, password);
+    axios
+      .post(
+        "http://localhost:80/api/test.php",
+        { email, password },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
+        }
+      )
+      .then((res) => console.log(res));
     e.preventDefault();
   };
 
@@ -11,11 +26,20 @@ export const LoginForm: React.FC<Props> = ({ switchSubmit }) => {
     <form className="login-popup-login-form">
       <section className="login-popup-input-container">
         <h5>電子信箱</h5>
-        <input type="text" placeholder="電子信箱" />
+        <input
+          type="text"
+          placeholder="電子信箱"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </section>
       <section className="login-popup-input-container">
         <h5>密碼</h5>
-        <input type="text" placeholder="密碼" />
+        <input
+          type="text"
+          placeholder="密碼"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </section>
       <button
         className="login-popup-button login-popup-login-submit"
