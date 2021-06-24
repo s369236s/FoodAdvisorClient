5c5b6130d4a1e890fc3401276e1fbf1fc6d1637f
s369236s <s369236s@gmail.com>
2021-06-24 16:10:58 +0800

    daw

--- src/apiKey.tsx
+++ src/apiKey.tsx
@@ -1,4 +1,4 @@
 const MODE = "dev";
 
 // export const SERVER_API_KEY = `http://foodadvisor.ddns.net:80/FoodAdvisorServer`;
-export const SERVER_API_KEY = `http://localhost:80/FoodAdvisorServer`;
+export const SERVER_API_KEY = `https://superfoodadvisor.herokuapp.com`;
